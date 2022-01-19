c54fc8852f39ca73bf59b7fc0e9639cc4f8c1166
Madalambana <almadalambana@gmail.com>
2022-01-19 04:44:14 +0200

    added procfile

--- app/psh.py
+++ app/psh.py
@@ -3,6 +3,7 @@ from sqlalchemy import engine
 from . import models
 from .database import engine
 from .routers import post, user, auth, vote
+from fastapi.middleware.cors import CORSMiddleware
 
 try:
     models.Base.metadata.create_all(bind=engine)
@@ -11,6 +12,17 @@ except Exception as error:
     print('error', error)
 
 app = FastAPI()
+
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
